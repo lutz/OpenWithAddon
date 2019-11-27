9d895544c5154ba76798b5d50abd594e7438946e
Daniel Lutz <daniel.lutz@outlook.com>
2019-11-27 19:38:29 +0100

    code init

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ license:    MIT
 
 ## Description
 
+This add-on adds in the context menu of a local file (tab **Title**) the command **Open with...** which calls the standard dialog of the Windows Explorer. This gives the possibility to open the files with another program than the standard application.
+
 ## Disclaimer
 
 >There are no support claims by the company **Swiss Academic Software GmbH**, the provider of **Citavi** or other liability claims for problems or data loss. Any use is at your own risk. All rights to the name **Citavi** and any logos used are owned by **Swiss Academic Software GmbH**.
---
+++ src/OpenWithAddon/Addon.cs
@@ -0,0 +1,68 @@
+﻿using Infragistics.Win.UltraWinToolbars;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Controls;
+using System;
+using System.Linq;
+
+namespace OpenWith
+{
+    public class Addon : CitaviAddOn<MainForm>
+    {
+        #region Constants
+
+        const string Key_Button_OpenWith = "SwissAcademic.Addons.OpenWith.Command";
+
+        #endregion
+
+        #region Fields
+
+        CommandbarButton _commandbarButton;
+
+        #endregion
+
+        #region Methods
+
+        public override void OnHostingFormLoaded(MainForm mainForm)
+        {
+            if (mainForm.ReferenceEditorElectronicLocationsToolbarsManager?.Tools.Cast<ToolBase>().FirstOrDefault(tool => tool.Key.Equals("ReferenceEditorUriLocationsContextMenu")) is PopupMenuTool popupMenu)
+                _commandbarButton = CommandbarMenu.Create(popupMenu).InsertCommandbarButton(4, Key_Button_OpenWith, Properties.OpenWithAddonResources.MenuCaption);
+
+
+            base.OnHostingFormLoaded(mainForm);
+        }
+
+        public override void OnLocalizing(MainForm mainForm)
+        {
+            if (_commandbarButton != null)
+                _commandbarButton.Text = Properties.OpenWithAddonResources.MenuCaption;
+
+            base.OnLocalizing(mainForm);
+        }
+
+        public override void OnApplicationIdle(MainForm mainForm)
+        {
+            if (_commandbarButton != null)
+                _commandbarButton.Visible = mainForm.GetPathOfSelectedElectronicLocations().IsNotNullOrEmptyOrWhiteSpace();
+
+            base.OnApplicationIdle(mainForm);
+        }
+
+        public override void OnBeforePerformingCommand(MainForm mainForm, BeforePerformingCommandEventArgs e)
+        {
+            if (e.Key.Equals(Key_Button_OpenWith, StringComparison.OrdinalIgnoreCase))
+            {
+                e.Handled = true;
+
+                Program.ClosePreview(mainForm.PreviewControl.ActiveUri);
+
+                var path = mainForm.GetPathOfSelectedElectronicLocations();
+                var args = $"{System.IO.Path.Combine(Environment.SystemDirectory, "shell32.dll")},OpenAs_RunDLL {path}";
+                System.Diagnostics.Process.Start("rundll32.exe", args);
+            }
+
+            base.OnBeforePerformingCommand(mainForm, e);
+        }
+
+        #endregion
+    }
+}
